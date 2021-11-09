b58db69b3356bacfb75d00c796a4b707d1185cd6
Andrew Sharkey <sharkand@hotmail.com>
2021-11-09 23:02:46 +0000

    Initial commit.

---
+++ src/main/java/com/sharkey/music/gigservice/GigserviceApplication.java
@@ -0,0 +1,13 @@
+package com.sharkey.music.gigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigserviceApplication.class, args);
+	}
+
+}
