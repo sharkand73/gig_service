86370c4f332bb8058a33edd915a10d11b3364f5a
Andrew Sharkey <sharkand@hotmail.com>
2022-06-05 23:28:11 +0100

    Trying to reboot app using comment in config file.

--- src/main/java/configurations/SpringGlobalConfig.java
+++ src/main/java/configurations/SpringGlobalConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// Adding this comment for new commit
+
 @Configuration
 public class SpringGlobalConfig implements WebMvcConfigurer {
 
