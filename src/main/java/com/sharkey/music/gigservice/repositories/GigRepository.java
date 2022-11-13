aed7c6b7df7ccd8958465fe6b781ce0d81b8235c
Andrew Sharkey <sharkand@hotmail.com>
2022-11-13 00:05:31 +0000

    Removed gig repository code

--- src/main/java/com/sharkey/music/gigservice/repositories/GigRepository.java
+++ src/main/java/com/sharkey/music/gigservice/repositories/GigRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface GigRepository extends JpaRepository<Gig, Long> {
-    public List<Gig> findByStartTimeYear(int year);
+    //public List<Gig> findByStartTimeYear(int year);
 }
