35a0e63d92a27e787c1bf64c60168d695eb80960
Andrew Sharkey <sharkand@hotmail.com>
2022-11-13 23:49:01 +0000

    Trying ordinal value for GigType

--- src/main/java/com/sharkey/music/gigservice/repositories/GigRepository.java
+++ src/main/java/com/sharkey/music/gigservice/repositories/GigRepository.java
@@ -11,6 +11,6 @@ public interface GigRepository extends JpaRepository<Gig, Long> {
     @Query("from Gig where YEAR(startTime)=?1")
     List<Gig> findByYear(int year);
 
-    @Query("select count (*) from Gig where YEAR(startTime)=?1 AND gigType<>'REHEARSAL'")
+    @Query("select count (*) from Gig where YEAR(startTime)=?1 AND gigType<>0")
     Long countByYear(int year);
 }
